b82976bfe49afaecd37f8e8428e4c0a1ce0b8638
gihantha <gihantha01@gmail.com>
2024-04-09 11:27:08 +0530

    Navbar feature implemented

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './Navbar.css'
+import logo from '../../Assets/logo.png'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt='logo' />
+      </div>
+      <ul className='nav-menu'>
+        <li>Services</li>
+        <li>About Us</li>
+        <li>Contacts</li>
+        <li>Careers</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar

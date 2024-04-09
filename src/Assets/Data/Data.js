940d888c0203f4ab47bfa43e8120218d23326d1c
gihantha <gihantha01@gmail.com>
2024-04-09 11:32:58 +0530

    Hero componet implement with assets

---
+++ src/Assets/Data/Data.js
@@ -0,0 +1,26 @@
+// data.js
+const data = [
+    {
+      question: 'onsectetur adipiscing elit?',
+      answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      question: 'voluptatem accusantium?',
+      answer: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.'
+    },
+    {
+      question: 'ipsum quia dolor sit amet?',
+      answer: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.'
+    },
+    {
+      question: 'sed do eiusmod tempor?',
+      answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      question: 'nim ad minima veniam',
+      answer: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.'
+    }
+  ];
+  
+  export default data;
+  
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./Hero.css";
+import hero_image from "../../Assets/hero_image.png";
+import hero_left from "../../Assets/hero_left.png";
+import hero_right from "../../Assets/hero_right.png";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-banner">
+        <img src={hero_image} alt="hero_image" />
+        
+      </div>
+      <div className="hero-left">
+        <img src={hero_left} alt="hero_left" />
+        <div className="hero-description">
+          <h3>Web & Mobile App Development</h3>
+          <p>d mobile Apps are pieces of the puzzle to grow your business. We use frameworks which tailor content and engagement methods to respond to different intents shown by your potential customers who interact with your business online.</p>
+          <button type="button" className="hero-btn">
+          LEARN MORE
+          </button>
+          
+        </div>
+      </div>
+      <div className="hero-right">
+      <div className="hero-description">
+          <h3>Digital Strategy Consulting</h3>
+          <p>Your digital strategy should complement the overall marketing strategy of the company. In online marketing, each component will never work in isolation and every business needs a different mix. We provide a clear concept and strategic overview to find the most efficient model for your business.</p>
+          <button type="button" className="hero-btn">
+          LEARN MORE
+          </button>
+          
+        </div>
+        <img src={hero_right} alt="hero_right" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
